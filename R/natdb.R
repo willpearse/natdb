eff2369d8501087ca3691c1262d4c5ce7f743bcb
Sylvia Kinosian <sylvia.kinosian@gmail.com>
2017-03-28 11:05:42 -0600

    stuff

--- R/natdb.R
+++ R/natdb.R
@@ -155,11 +155,11 @@ traits <- function(x, ...){
      if(!inherits(x, "natdb"))
          stop("'", deparse(substitute(x)), "' must be of type 'natdb'")
      # load in bibtex file
-     bib <- readRDS("citations.rdata")
+     bib <- readRDS("~/Documents/natdb/data/natdb_citations.RData")
      # create column with just the function names (.name.year)
      bib$Name <- paste0(".", tolower(bib$Author), ".", bib$Year)
-     # 
-      datasets <- Filter(Negate(is.function), ls(pattern="^\\.[a-z]*\\.[0-9]+", name="package:natdb", all.names=TRUE))
+     # find referenced function, get name, return bibtex
+     datasets <- Filter(Negate(is.function), ls(pattern="^\\.[a-z]*\\.[0-9]+", name="package:natdb", all.names=TRUE))
  
      return(bib[which(bib$Name=='xxxxxx'),3]) 
  }
